7b43ba656557e61dc109acb64204156c2c2b86c8
João Ricardo Mesquita <123390746+JothaSky@users.noreply.github.com>
2023-05-08 19:06:42 -0300

    funcionando com while
    feito laço de repetição com while

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-document.querySelector('.tecla_pom');
+function tocaSom() {
+    document.querySelector("#som_tecla_pom").play();
+    
+}
+function tocaSom(idElementaudio) {
+    document.querySelector(idElementaudio).play();
+}
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+
+//enquanto
+while (contador < ListaDeTeclas.length) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+ console.log(idAudio);
+
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+    contador++;
+
+    console.log(contador);
+}
